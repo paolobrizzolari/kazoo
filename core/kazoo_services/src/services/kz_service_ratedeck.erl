d9f1f7635829815c6823f6a92718160fd5b1f401
James Aimonetti <james.aimonetti@gmail.com>
2017-01-25 21:35:38 +0000

    fix spelling

--- core/kazoo_services/src/services/kz_service_ratedeck.erl
+++ core/kazoo_services/src/services/kz_service_ratedeck.erl
@@ -25,7 +25,7 @@
 -spec reconcile(kz_services:services(), api_binary()) -> kz_services:services().
 reconcile(Services) ->
     PlanJObj = kz_services:service_plan_json(Services),
-    %% TODO: resolve conflict when there is more then one ratedeck
+    %% TODO: resolve conflict when there is more than one ratedeck
     case kz_json:get_keys(kz_json:get_json_value([<<"plan">>, ?SERVICE_CATEGORY], PlanJObj, kz_json:new())) of
         [] -> kz_services:reset_category(?SERVICE_CATEGORY, Services);
         [RatedeckId] -> kz_services:update(?SERVICE_CATEGORY, RatedeckId, 1, Services);
